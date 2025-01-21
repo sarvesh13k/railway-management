815df84d5e2fc32786662e8e85b839f865ce53f8
Sarvesh Kothule <sarvesh.kothule97@gmail.com>
2025-01-21 12:06:03 +0530

    Remove all existing files

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'Sarveshsql97@'
-MYSQL_DB = 'railway_management'
-MYSQL_PORT = 3800 
--- src/api.js
+++
@@ -1,8 +0,0 @@
-// src/api.js
-import axios from 'axios';
-
-const API = axios.create({
-  baseURL: 'http://localhost:5000', // Flask server URL
-});
-
-export default API;
